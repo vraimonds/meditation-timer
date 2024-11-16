ba99b2d888f73010e11fd35f447c52c30fa9ec44
Raimonds Vanags <raimonds.vanags@deepl.com>
2024-11-16 19:56:55 +0100

    Removing artifacts from the background-video and further reducing it's size.

--- assets/reverse-video.sh
+++ assets/reverse-video.sh
@@ -1,3 +1,3 @@
-# ffmpeg -i 234450_small.mp4 -t 10 -c copy background-video.mp4 # optional cut the video 30 sec short 
-ffmpeg -i background-video.mp4 -vf reverse -af areverse background-video-reversed.mp4
-ffmpeg -f concat -safe 0 -i file_list.txt -c copy combined.mp4
+# ffmpeg -i 234450_small.mp4 -t 10 -c copy background-video-short.mp4 # optional cut the video 30 sec short 
+ffmpeg -i background-video-short.mp4 -vf reverse -af areverse background-video-reversed.mp4
+ffmpeg -f concat -safe 0 -i file_list.txt -c copy background-video.mp4
